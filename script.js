e41aef568b4661083f8ffd61ab4ba7b8574dff61
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-03-14 01:56:44 -0300

    added showError function

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPass = document.getElementById('confirm-pass');
 
+// Show input error message
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.classList.add('form__control--error');
+  const error = formControl.querySelector('.form__error');
+  error.textContent = message;
+  error.classList.add('form__error--visible');
+};
+
+// Event listeners
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
